a17571fe60375cae94570ea7967fa4927c8f7ba8
tide <87511793+Tide-Matrix@users.noreply.github.com>
2022-02-17 18:47:26 +0000

    Update coinflip.js
    Edited coinflip.js to allow user to select heads or tails (args: <amount> <heads or tails>)

--- commands/Currency/coinflip.js
+++ commands/Currency/coinflip.js
@@ -7,26 +7,52 @@ module.exports = {
     description:
       "Flip a coin. Bet an amount of money that either doubles or is lost.",
     args: true,
-    usage: "<amount>",
+    usage: "<amount>, <heads or tails>",
     useCurrency: true,
     async execute(message, args, currency) {
         // generate true or false randomly
         const bet = args[0];
-        const max = 1;
-		const number = Math.floor(Math.random() * (max + 1));
+	const guess = args[1];
+	var heads = true; //if the coin was heads
+	var correct = false; // Was the guess correct
+    const max = 2
+    const number = Math.floor(Math.random() * max);
 
         if (bet > currency.getBalance(message.author.id)) {
             return message.channel.send(`Sorry ${message.author}, but you can't make that bet since you only have ${currencyPrefix}${currency.getBalance(message.author.id)}.`);
         }
+        if (guess === undefined)
+            return message.channel.send(`Please specify if you want heads or tails ${message.author}. Arguments are <amount> <heads or tails> `)
+	//decide if heads or tails
+	if (number === 1) {
+		heads = true;
+	} else {
+		heads = false
 
-        if (number === 1) {
+	//check if correct
+	if (heads === true && guess === 'heads') {
+		correct = true;
+	} else if (heads === false && guess === 'tails') {
+		correct = true;
+    } else {
+          correct = false;
+      }
+	}
+        if (correct === true && heads === true) {
             currency.add(message.author.id, bet * 2);
-            return message.channel.send(`Heads!\nYour balance: ${currencyPrefix}${currency.getBalance(message.author.id)}`);
-        } else {
-            currency.add(message.author.id, -bet);
-            return message.channel.send(`Tails...\nYour balance: ${currencyPrefix}${currency.getBalance(message.author.id)}`);
+            return message.channel.send(`It was heads!\nYour balance: ${currencyPrefix}${currency.getBalance(message.author.id)}`);
+        } else if (correct === false && heads === true){
+            currency.add(message.author.id, bet * -1);
+            return message.channel.send(`It was heads...\nYour balance: ${currencyPrefix}${currency.getBalance(message.author.id)}`);
+        } else if (correct === true && heads === false){
+	    currency.add(message.author.id, bet * 2);
+	    return message.channel.send(`It was tails!\nYour balance: ${currencyPrefix}${currency.getBalance(message.author.id)}`);
+	} else {
+	    currency.add(message.author.id, bet * -1);
+	    return message.channel.send(`It was tails...\nYour balance: ${currencyPrefix}${currency.getBalance(message.author.id)}`);
         }
-        // true: double amount, give to User
-        // false: subtract amount from User
-    },
-};
+
+        // incorrect: double amount, give to User
+        // correct: subtract amount from User
+    }
+}
