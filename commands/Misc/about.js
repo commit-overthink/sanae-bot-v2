5a8a192b1c93117fcf26b8520ec8028d79d7dce0
commit-overthink <71522316+commit-overthink@users.noreply.github.com>
2022-02-19 19:24:31 -0500

    changed about embed

--- commands/Misc/about.js
+++ commands/Misc/about.js
@@ -7,8 +7,8 @@ module.exports = {
 	execute(message) {
 		const embed = new Discord.MessageEmbed()
 			.setColor(defaultEmbedColor)
-			.setTitle("About Sanae")
-			.setURL("https://github.com/origami-matrix/sanae-bot-v2")
+			.setTitle("Check out my Roadmap!")
+			.setURL("https://github.com/pheon1x76/sanae-v2/projects/1")
 			.setDescription(
 				`Bot made by phoen1x239#9739.\nPlease DM me for command ideas.\nType ${prefix}help to see a list of commands.`,
 			);
